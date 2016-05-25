647c36e0a3846ccedd318fe9af836c6ce4d6e290
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-25 15:50:10 -0600

    full lists controller

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,53 @@
+class ListsController < ApplicationController
+  def index
+  	@lists = List.all
+  end
+
+
+  def show
+  	@list = List.find(params[:id])
+  end
+
+
+  def new
+  	@list = List.new
+  end
+
+  def create
+  	@list = List.new(list_params)
+  	if @list.save
+  		redirect_to lists_path
+	else
+		render :new
+	end
+  end
+
+
+  def edit
+  	@list = List.find(params[:id])
+  end
+
+  def update
+  	@list = List.find(params[:id])
+  	if @list.update(list_params)
+  		redirect_to list_path(@list) 
+	else
+		render :edit
+	end
+  end
+
+
+
+  def destroy
+  end
+
+
+  private
+
+
+  def list_params
+  	params.require(:list).permit(:name, :description)
+  end
+
+
+end
